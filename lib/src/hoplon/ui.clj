eca9edfda3d207a8e03f74adf59833d1eec209e2
jumblerg <jumblerg@gmail.com>
2017-03-27 08:51:46 -0400

    eliminate elem macros to facilitate better composition
    by utilizing inherited vars that set up their bindings based on
    their element's position in the dom rather than dynamic vars set
    by macros to create bindings based on a function's lexical
    placment when a file is evaluated, macros no longer need to be
    used to construct elems that require dom inheritance. since cljs
    macros cannot be evaluated at runtime, previously, a new macro had
    to be created whenever a higher-level elem was being constructed
    from a primitives that (ab)used dynamic bindings to pass values to
    childen.  now that they're functions, this is no longer an issue.

--- lib/src/hoplon/ui.clj
+++ lib/src/hoplon/ui.clj
@@ -8,22 +8,6 @@
 (defmacro bind-in! [elem path value]
   `(bind-with! (fn [v#] (set-in! ~elem ~path v#)) ~value))
 
-(defmacro cmpt [& args]
-  `(hoplon.binding/binding [hoplon.ui/*pointer* (javelin.core/cell {:over 0 :down 0 :up 0 :out 0})
-                            hoplon.ui/*state*   (javelin.core/cell nil)]
-    (cmpt* ~@args)))
-
-(defmacro item [& args]
-  `(hoplon.binding/binding [hoplon.ui/*pointer* (javelin.core/cell {:over 0 :down 0 :up 0 :out 0})
-                            hoplon.ui/*state*   (javelin.core/cell nil)]
-    (item* ~@args)))
-
-(defmacro form [& args]
-  `(hoplon.binding/binding [*data*   (javelin.core/cell {})
-                            *error*  (atom nil)
-                            *submit* (atom nil)]
-    (form* ~@args)))
-
 #_(defn slurp-bytes [x]
   (with-open [out (java.io.ByteArrayOutputStream.)]
     (clojure.java.io/copy (clojure.java.io/input-stream (clojure.java.io/resource x)) out)
