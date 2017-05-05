a17ffa4abb0ac2e77b4adf67f2f45c7006c6fdf4
jumblerg <jumblerg@gmail.com>
2017-05-05 01:25:50 -0400

    add form to test app

--- app/src/hoplon/ui/app.cljs
+++ app/src/hoplon/ui/app.cljs
@@ -4,11 +4,9 @@
   (:require
     [javelin.core         :refer [cell= cell]]
     [hoplon.core          :refer [defelem for-tpl when-tpl case-tpl]]
-    [hoplon.ui            :refer [window elem path line-path image video b t ]]
+    [hoplon.ui            :refer [window elem line lines path line-path image video b t ]]
     [hoplon.ui.attrs      :refer [- r font hsl rgb]]
-    [hoplon.ui.transforms :refer [linear]])
-  (:require-macros
-    [hoplon.ui.app   :refer [view-tpl]]))
+    [hoplon.ui.transforms :refer [linear]]))
 
 ;;; utils ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -40,6 +38,7 @@
 ;-- colors --------------------------------------------------------------------;
 
 (def black  (rgb 0x1F1F1F))
+(def grey   (rgb 0xCCCCCC))
 (def orange (rgb 0xE73624))
 (def blue   (rgb 0x009BFF))
 (def yellow (rgb 0xF5841F))
@@ -49,13 +48,21 @@
 
 (def baloo (font :truetype "baloo.ttf" :generic :sans-serif))
 
-(def +menu-text+  {:t 18 :tf baloo :tc :white :tw 1})
-(def +label-text+ {:t 16 :tf baloo :tc :white :tw 1})
+(def +title+ {:t 26 :tf baloo :tc black :tw 2})
+(def +menu+  {:t 18 :tf baloo :tc white :tw 1.5})
+(def +label+ {:t 16 :tf baloo :tc white :tw 1})
+(def +field+ {:t 24 :tf baloo :tc black :tw 1})
+
+;-- attributes -----------------------------------------------------------------
+
+(def -button- {:ph 12 :pv 6 :r 3 :a :mid :c grey :b 2 :bc black :m :pointer})
+(def -field-  {:ph 12 :pv 6 :r 3 :c grey :b 2 :bc black})
 
 ;;; content ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def menu-items
-  [:transitions "transitions"
+  [:forms       "forms"
+   :transitions "transitions"
    :scales      "scales"
    :media       "media"])
 
@@ -82,29 +89,43 @@
       (video :s 200 :a :mid :b 2 :bc :black :fit :contain :autoplay :true :url "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"
         (elem "contained")))))
 
+(defn forms-view []
+  (elem :sh (r 1 1) :p g
+    (elem +title+ :sh (r 1 1)
+      "Forms")
+    (let [data (cell (or (:data sess) {}))]
+      (cell= (prn :data data))
+      (elem +label+ :s (r 1 1) :p (b 16 sm 50) :g 16 :ah :end
+        (line  -field- +field+ :sh (r 1 1)          :prompt "Name"    :src (path= data [:name]))
+        (line  -field- +field+ :sh (r 1 1)          :prompt "Email"   :src (path= data [:email]))
+        (lines -field- +field+ :sh (r 1 1) :rows 10 :prompt "Message" :src (path= data [:message]))
+        (elem  -button- +field+ :sh (>sm 400) :click #(swap! sess assoc :data data)
+          "Submit")))))
+
 (defn scales-view []
   (let [xs (mapv (partial * 30) (range 21))]
     (elem :sh (r 1 1) :sv (- (r 1 1) 80) :p g :a :mid
       (elem :s 600 :b 4 :bc :grey :d :pile
         (for [[index [label function]] (map-indexed vector (partition 2 transforms))]
-          (path +label-text+ :s (r 1 1) :b 600 :p g :k 4 :kc (hsl (* (count transforms) 20) (r 1 2) (r 1 2)) :av :mid
+          (path +label+ :s (r 1 1) :b 600 :p g :k 4 :kc (hsl (* (count transforms) 20) (r 1 2) (r 1 2)) :av :mid
             :src (mapcat vector xs (mapv (linear [0 600] [0 -600]) xs))))))))
 
 (defn transitions-view []
   (let [size (cell 150)]
     (elem :sh (r 1 1) :p g :g g
       (for [[index [label function]] (map-indexed vector (partition 2 transforms))]
-        (elem +label-text+ :sh (t size 1000 function) :sv 60 :p g :c (hsl (* (count transforms) 20) (r 1 2) (r 1 2)) :av :mid :m :pointer :click #(swap! size (partial + 150))
+        (elem +label+ :sh (t size 1000 function) :sv 60 :p g :c (hsl (* (count transforms) 20) (r 1 2) (r 1 2)) :av :mid :m :pointer :click #(swap! size (partial + 150))
           label)))))
 
 (window :src route :title "Hoplon UI"
-  (elem :sh (r 1 1) :sv 80 :av :mid :p g :g g :c orange :bt 8 :bc yellow
-    (image :s 40 :m :pointer :click #(reset! state :home) :url "hoplon-logo.png")
+  (elem :sh (r 1 1) :sv 80 :av :mid :p g :g g :c orange :bt 4 :bc yellow
+    (image :s 50 :m :pointer :click #(reset! state :home) :url "hoplon-logo.png")
     (elem :sh (>sm (- (r 1 1) (+ 60 g))) :g g :ah :end
       (for [[*state label] (partition 2 menu-items) :let [sel (cell= (= *state state))]]
-        (elem +menu-text+ :sh (>sm :auto) :m :pointer :bb (cell= (when sel 3)) :bc :white :click #(reset! state *state)
+        (elem +menu+ :sh (>sm :auto) :m :pointer :bb (cell= (when sel 3)) :bc :white :click #(reset! state *state)
           label))))
   (case-tpl state
+    :forms       (forms-view)
     :media       (media-view)
     :scales      (scales-view)
     :transitions (transitions-view)))
